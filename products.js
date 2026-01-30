02b8f02bb5f695a78d3cc1ee9a2328a8087aa83f
muhibbahalliance7-sys <muhibbahalliance7@gmail.com>
2026-01-30 16:44:35 +0800

    Create products.js

---
+++ products.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    name: "COSHARE 6.65%",
+    dsr: 0.60,
+    minTenure: 1,
+    maxTenure: 15,
+    retirementAge: 60
+  },
+  {
+    name: "COSHARE 8%",
+    dsr: 0.65,
+    minTenure: 1,
+    maxTenure: 12,
+    retirementAge: 58
+  },
+  {
+    name: "KOPRIMAS",
+    dsr: 0.70,
+    minTenure: 1,
+    maxTenure: 10,
+    retirementAge: 55
+  }
+  // 👉 ADD remaining products here
+];
